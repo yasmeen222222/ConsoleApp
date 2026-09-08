fa16fd5b0ba91cdd90e54696597e185f6cf49a03
YASMEEN <YASMEEN@DESKTOP-45683ES>
2026-09-08 23:06:16 +0300

    question 2

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -16,7 +16,13 @@ namespace ConsoleApp4
             //double[] prices = { 25.5, 40.0, 33.75 };
             //Console.WriteLine(prices[1]);
             #endregion
+            #region question 2
+            //            Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+            //shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+            //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            //Console.WriteLine(shelfCopies[1, 0]);
 
+            #endregion
 
         }
 
