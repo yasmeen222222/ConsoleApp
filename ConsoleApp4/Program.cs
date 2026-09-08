a1572bbe0f1b27f8476eaff3ef0f4209cfc82929
YASMEEN <YASMEEN@DESKTOP-45683ES>
2026-09-08 22:54:55 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+
+        
+        
+            static void Main(string[] args)
+        {
+
+        }
+
+        
+    }
+}
