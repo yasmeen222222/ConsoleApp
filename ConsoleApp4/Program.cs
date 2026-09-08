2fc73aafdf7f9fb8a9487d0ec7274c3aa419c520
YASMEEN <YASMEEN@DESKTOP-45683ES>
2026-09-08 23:01:17 +0300

    question 1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ConsoleApp4
 {
@@ -9,9 +10,16 @@ namespace ConsoleApp4
         
             static void Main(string[] args)
         {
+            #region question 1
+            //Create a one - dimensional array double[] prices with the values 25.5, 40.0, 33.75.Print
+            //the second price(index 1).
+            //double[] prices = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(prices[1]);
+            #endregion
+
 
         }
 
-        
+
     }
 }
